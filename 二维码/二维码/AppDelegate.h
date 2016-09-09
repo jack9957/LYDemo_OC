9de82158e35928ebf337bcaf664437ce8ae57867
liyang.github.io <995720636@qq.com>
2016-09-09 11:16:41 +0800

    二维码

---
+++ 二维码/二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码
+//
+//  Created by liyang on 16/9/9.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
