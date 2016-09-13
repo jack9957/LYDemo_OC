a247a509f6bf29e39f1ca587187bb3e79dfa72c0
liyang.github.io <995720636@qq.com>
2016-09-13 10:02:47 +0800

    1、富文本学习

--- AttributeString/AttributeString/ViewController.m
+++ AttributeString/AttributeString/ViewController.m
@@ -9,19 +9,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+ /**
+ iOS 6之前：CoreText,纯C语言,极其蛋疼
+ iOS 6开始：NSAttributedString,简单易用
+ iOS 7开始：TextKit,功能强大,简单易用
+ */
+
+
+
+
+- (IBAction)btn1Action:(id)sender
+{
+    // 1、学习练习使用富文本之NSAttributedString
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:@"学习练习使用富文本"];
+    
+    // 2、设置各种属性
+    [string addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0, 4)];
+    [string addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:30] range:NSMakeRange(0, 4)];
+    
+    [string addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(4, string.length-4)];
+    [string addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:10] range:NSMakeRange(4, string.length-4)];
+    
+    self.testLabel.attributedText = string;
 }
 
 @end
